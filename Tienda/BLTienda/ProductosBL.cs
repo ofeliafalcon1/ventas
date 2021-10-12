e0a0bbaa2f35d46fcb0c5a9d3c35b5a4ab75115d
ofeliafalcon1 <ofelia.falcon@unah.hn>
2021-10-11 18:54:24 -0600

    Instanciando una lista Binding y agregando datos de prueba para los productos.

--- Tienda/BLTienda/ProductosBL.cs
+++ Tienda/BLTienda/ProductosBL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,65 @@ namespace BLTienda
 {
     public class ProductosBL
     {
+        /*Creacion de Lista Binding (arreglo) para los productos*/
+        BindingList<Producto> ListaProductos;
+
         public ProductosBL()
         {
+            /*Instanciamos la lista en nuestro inicializador*/
+            ListaProductos = new BindingList<Producto>();
 
+            /*Instanciamos productos en la lista y colocamos todos 
+             los datos del producto en sus propiedades.*/
+             
+            //Producto1
+            var Producto1 = new Producto();
+            Producto1.ID = 1;
+            Producto1.Descripcion = "Camisetas";
+            Producto1. Precio = 65;
+            Producto1.Existencia = 100;
+            Producto1.Activo = true;
+
+            ListaProductos.Add(Producto1);
+
+            //Producto2
+            var Producto2 = new Producto();
+            Producto2.ID = 2;
+            Producto2.Descripcion = "Vestidos";
+            Producto2.Precio = 1500;
+            Producto2.Existencia = 15;
+            Producto2.Activo = true;
+
+            ListaProductos.Add(Producto2);
+
+            //Producto3
+            var Producto3 = new Producto();
+            Producto3.ID = 3;
+            Producto3.Descripcion = "Jeans";
+            Producto3.Precio = 600;
+            Producto3.Existencia = 50;
+            Producto3.Activo = true;
+
+            ListaProductos.Add(Producto3);
+
+            //Producto4 
+            var Producto4 = new Producto();
+            Producto1.ID = 4;
+            Producto1.Descripcion = "Calcetines";
+            Producto1.Precio = 95;
+            Producto1.Existencia = 20;
+            Producto1.Activo = true;
+
+            ListaProductos.Add(Producto4);
+
+        /*Obtencion de Productos*/
         }
+        public BindingList<Producto> ObtenerProductos()
+        {
+            return ListaProductos;
+        }
+
+        /*Propiedades de los productos*/
       }
     public class Producto
     {
