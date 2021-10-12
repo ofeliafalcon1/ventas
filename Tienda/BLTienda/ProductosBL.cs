1e03032f7fce4c0ff7eb009e15a8d658d56f9580
ofeliafalcon1 <ofelia.falcon@unah.hn>
2021-10-11 18:04:23 -0600

    Actualizando la clase de BLProductos con sus propiedades

---
+++ Tienda/BLTienda/ProductosBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLTienda
+{
+    public class ProductosBL
+    {
+        public ProductosBL()
+        {
+
+        }
+      }
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public bool Activo { get; set; }
+      }
+   }
+    
